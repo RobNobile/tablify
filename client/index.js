b444066cadf7a2000220de1446156946bdfef4d1
Rob Nobile <robert.nobile@gmail.com>
2020-08-11 16:54:22 -0400

    Initialize css

--- client/index.js
+++ client/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import { render } from 'react-dom';
 
+import styles from './styles.css';
+
 import Header from './containers/Header.jsx';
 import Main from './containers/Main.jsx';
 
--- webpack.config.js
+++ webpack.config.js
@@ -32,19 +32,19 @@ module.exports = {
           }
         }
       },
-      // {
-      //   // rules for bundling .scss/.css files
-      //   test: /\.s?css/,
-      //   exclude: /node_modules/,
-      //   use: [ // use as an array of multiple loaders
-      //     // Creates `style` nodes from JS strings
-      //     'style-loader',
-      //     // Translates CSS into CommonJS
-      //     'css-loader',
-      //     // Compiles Sass to CSS
-      //     'sass-loader',
-      //   ],
-      // },
+      {
+        // rules for bundling .scss/.css files
+        test: /\.s?css/,
+        exclude: /node_modules/,
+        use: [ // use as an array of multiple loaders
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // // Compiles Sass to CSS
+          // 'sass-loader',
+        ],
+      },
     ]
   },
   plugins: [
