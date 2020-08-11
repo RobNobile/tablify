dfa5bad4aed6035378d8a222ac84e143c51983bf
Rob Nobile <robert.nobile@gmail.com>
2020-08-11 15:06:38 -0400

    Setup webpack

---
+++ client/index.js
@@ -0,0 +1,6 @@
+import sum from './sum';
+
+const greet = name => console.log(`Hello ${name}!`);
+greet('Rob');
+
+console.log('TestLog!!!', sum(3, 3));
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  // entry point from which webpack will build its dependency graph // or use path.resolve(__dirname, './clinet/index.js')
+  entry: path.resolve(__dirname, './client/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    // consider filename: 'js/[name].[hash].js', for caching - https://www.sentinelstand.com/article/create-react-app-from-scratch-with-webpack-and-babel
+    filename: './[name].[hash].js',
+    // make the build folder a public path
+    // publicPath: '/dist/',
+  },
+  devServer: {
+    // The dev server will serve content from this directory.
+    contentBase: path.resolve(__dirname, 'client'),
+    hot: true,
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        // rules for bundling .js/.jsx files
+        test: /\.jsx?/,
+        // exclude node modules
+        exclude: /node_modules/,
+        // use babel loader with react specific presets to transpile JSX/React/ES6+ code down to ES5
+        use: { // use as an object if one loader
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          }
+        }
+      },
+      // {
+      //   // rules for bundling .scss/.css files
+      //   test: /\.s?css/,
+      //   exclude: /node_modules/,
+      //   use: [ // use as an array of multiple loaders
+      //     // Creates `style` nodes from JS strings
+      //     'style-loader',
+      //     // Translates CSS into CommonJS
+      //     'css-loader',
+      //     // Compiles Sass to CSS
+      //     'sass-loader',
+      //   ],
+      // },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './client/index.html'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
